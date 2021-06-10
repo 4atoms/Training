d24bd886ebfb97172e633f50a176484021ad4c67
Manish Kumar <manishkumar222mani@gmail.com>
2021-06-10 11:49:33 +0530

    adding form and displaying input on console

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
   }
+
+  var form = document.getElementById("form");
+  form.addEventListener('submit', function(event){
+      event.preventDefault();
+
+      var userfirstname=document.getElementById("userfirstname").value
+      console.log(userfirstname)
+      var userlastname = document.getElementById("userlastname").value
+      console.log(userlastname)
+  })
