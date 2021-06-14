57ef92a58c8e494fb32dc337f626350c4fde2227
shivanics96 <83757527+shivanics96@users.noreply.github.com>
2021-06-14 11:59:23 +0530

    Add files via upload

---
+++ Training task2/listFlight.js
@@ -0,0 +1,15 @@
+ const flightData=()=>{
+    const flightList=[
+        {name:'Indigo',seats:50},
+        {name:'Air India',seats:100},
+        {name:'Spice Jet',seats:150},
+        {name:'Go Air',seats:200},
+        {name:'Air Asia India',seats:250},
+        {name:'Vistara',seats:150},
+        {name:'Alliance',seats:50},
+        {name:'Truset',seats:70}
+    ];
+    for(var i=0;i<flightList.length;i++){
+        console.log(flightList[i]);
+    }
+ }
