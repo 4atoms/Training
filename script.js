77340864602f157507827640e784e58775fa8015
Manish Kumar <manishkumar222mani@gmail.com>
2021-06-15 08:13:36 +0530

    hard coded value of seat

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function togglePopup(){
 
 
   var arr=[
-    {name:"SpiceJet",num:36},
+    {name:"SpiceJet",num:54},
+    
 ];
 var element=document.getElementById("data");
 
 for (var i=0;i<arr.length;i++){
     var div=document.createElement("div");
    
-    div.innerHTML='<br>Flight Name: <h2>' +arr[i].name + '</h2> having '+arr[i].num +' Seat Check Availability ...' 
+    div.innerHTML= '<h2>' +arr[i].name + '</h2> having '+arr[i].num +' Seat Check Availability ...' 
     element.appendChild(div);
 }
 
