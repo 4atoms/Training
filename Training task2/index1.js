6e7a323b9f131b8f014a8bec252f49fc55c5bd18
shivanics96 <83757527+shivanics96@users.noreply.github.com>
2021-06-18 10:04:58 +0530

    Add files via upload

---
+++ Training task2/index1.js
@@ -0,0 +1,141 @@
+class flightNameslist{
+    constructor(){
+    this.element=document.getElementById("data");
+    this.flights=document.getElementById("flights");
+    this.elementRemo=document.getElementById("clickId");
+    }    
+     displayFlights = () => {
+        flights.removeChild(this.elementRemo)
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET","https://run.mocky.io/v3/782a2a9c-cbbc-4995-b304-a41303a58121");
+        
+        xmlHttp.onload = (resp) => {
+            const { response } = resp.target;
+            var JSONResp = JSON.parse(response);
+            JSONResp.forEach(f => {
+                var outerElement = document.createElement("div")
+                outerElement.classList.add('outerEle');
+                var elem = document.createElement("div");
+                var but = document.createElement("button");
+                but.textContent = "DetailS"
+                but.classList = "displayButton"
+                but.onclick = function(){flight.checkAvailability(f.url)}
+                elem.classList.add('flightNames')
+                elem.innerHTML = "Flight : "+f.name;
+                outerElement.appendChild(elem); 
+                outerElement.appendChild(but);         
+                this.element.appendChild(outerElement);        
+            });
+        };
+        
+        xmlHttp.send();
+    }
+    
+     checkAvailability = (url) => {
+    
+        flight.closePopup();
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", url,false);
+        var capacity;
+        xmlHttp.onload = (resp) => {
+            const { response } = resp.target;
+            var JSONResp = JSON.parse(response);
+            capacity = JSONResp.seats;
+        };
+        xmlHttp.send();
+    
+        var parent = document.createElement("div")
+        parent.id = "parent"
+        parent.style.display = "block"
+        parent.classList.add('popup')
+    
+        var closebutton = document.createElement("button")
+        closebutton.classList.add('closebutton')
+        closebutton.onclick = function() {flight.closePopup()}
+        closebutton.innerText = "X"
+    
+        parent.appendChild(closebutton)
+        var i=1;
+        while(i<=capacity){
+    
+            var par = document.createElement("div")
+            par.classList.add('parent')
+        
+            var innerdiv1 = document.createElement("div")
+            innerdiv1.classList.add('innerdivEnd')
+            
+            var innerdiv11 = document.createElement("div")
+            innerdiv11.classList.add('innerDivEnd1')
+            if(i <= capacity) innerdiv11.innerHTML = i++;
+            innerdiv1.appendChild(innerdiv11)
+    
+            var innerdiv12 = document.createElement("div")
+            innerdiv12.classList.add('innerDivEnd1')
+            if(i <= capacity) innerdiv12.innerHTML = i++;
+            innerdiv1.appendChild(innerdiv12)
+    
+            var innerdiv13 = document.createElement("div")
+            innerdiv13.classList.add('innerDivEnd1')
+            if(i <= capacity) innerdiv13.innerHTML = i++;
+            innerdiv1.appendChild(innerdiv13)
+    
+            par.appendChild(innerdiv1)
+            var innerdiv2 = document.createElement("div")
+            innerdiv2.classList.add('innerdivMid')
+            
+            var innerdiv21 = document.createElement("div")
+            innerdiv21.classList.add('innerDivMid1')
+            if(i <= capacity) innerdiv21.innerHTML = i++;
+            innerdiv2.appendChild(innerdiv21)
+    
+            var innerdiv22 = document.createElement("div")
+            innerdiv22.classList.add('innerDivMid1')
+            if(i <= capacity) innerdiv22.innerHTML = i++;
+            innerdiv2.appendChild(innerdiv22)
+    
+            var innerdiv23 = document.createElement("div")
+            innerdiv23.classList.add('innerDivMid1')
+            if(i <= capacity) innerdiv23.innerHTML = i++;
+            innerdiv2.appendChild(innerdiv23)
+    
+            var innerdiv24 = document.createElement("div")
+            innerdiv24.classList.add('innerDivMid1')
+            if(i <= capacity) innerdiv24.innerHTML = i++;
+            innerdiv2.appendChild(innerdiv24)
+    
+            par.appendChild(innerdiv2)
+            var innerdiv3 = document.createElement("div")
+            innerdiv3.classList.add('innerdivEnd')
+            
+            var innerdiv31 = document.createElement("div")
+            innerdiv31.classList.add('innerDivEnd1')
+            if(i <= capacity) innerdiv31.innerHTML = i++;
+            innerdiv3.appendChild(innerdiv31)
+            
+            var innerdiv32 = document.createElement("div")
+            innerdiv32.classList.add('innerDivEnd1')
+            if(i <= capacity) innerdiv32.innerHTML = i++;
+            innerdiv3.appendChild(innerdiv32)
+    
+            var innerdiv33 = document.createElement("div")
+            innerdiv33.classList.add('innerDivEnd1')
+            if(i <= capacity) innerdiv33.innerHTML = i++;
+            innerdiv3.appendChild(innerdiv33) 
+    
+            par.appendChild(innerdiv3)    
+        
+            parent.appendChild(par)
+        }
+    
+        this.element.appendChild(parent)
+    }
+    
+     closePopup =() => {
+        const pop = document.getElementById("parent")
+        if(pop != null){
+            pop.style.display = "none"
+            this.element.removeChild(pop)
+        }
+    }
+}
+const flight=new flightNameslist();
